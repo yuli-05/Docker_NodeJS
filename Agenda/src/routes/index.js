08f4337528cd80f313f2e7f5889dd4d6e6ac5304
yuli-05 <1718110384@utectulancingo.edu.mx>
2021-08-13 08:08:00 +0000

    Formulario con firebase

--- Agenda/src/routes/index.js
+++ Agenda/src/routes/index.js
@@ -1,20 +1,58 @@
-const express = require("express");
+ express = require("express");
 
 const app = express();
+var exphbs = require('express-handlebars')
+var bodyParser= require('body-parser')
 
-app.get("/", (req, res) => {
-  res.render('index.hbs')
+app.use(bodyParser.urlencoded({extended: true }))
+app.use(express.static('public'))
+
+//cambie variable
+const admin =  require('firebase-admin');
+
+var serviceAccount = require("/workspace/Docker_NodeJS/Agenda/agenda2-1930b-firebase-adminsdk-wv3ia-21e2881362.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://agenda2-1930b-default-rtdb.firebaseio.com/"
 });
 
- app.post('/new-contact', (req, res) => {
-     console.log(req.body);
-     res.send('received');
+const db = admin.database();
+
+app.get('/', (req, res) => {
+    db.ref('agenda').once('value', (snapshot) => {
+        const data = snapshot.val();
+        res.render('index.hbs', { agenda: data });
+    });
+  //res.render('index.hbs')
+});
+
+ /*app.post('/new-contact', (req, res)  => {
+     console.log();
+     /*const newCon = {
+         name: req.body.name,
+         email: req.body.email
+     };*/
+     //db.ref('Agenda').push(req.body);
+     //res.send('received');
+//});
+
+app.post('/guardar', (req, res) => {
+    console.log(req.body);
+    db.ref('agenda').push(req.body);
+    res.redirect('/');
+});
+
+app.get('/delete/:id', (req, res) => {
+    db.ref('agenda/' + req.params.id).remove();
+    res.redirect('/');
 });
 
 app.listen(3000, () => {
   console.log("App running on port 3000...");
 });
 
+module.exports = app;
 
 
 /*const { Router } = require('express');
@@ -26,4 +64,6 @@ router.get('/', (req, res) => {
 });*/
   
 
-module.exports = app;
+
+
+
