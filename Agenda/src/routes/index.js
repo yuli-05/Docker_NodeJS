1a36ff59203af7640536765b0f4d7407d80c1941
yuli-05 <1718110384@utectulancingo.edu.mx>
2021-08-15 01:44:36 +0000

    login con auth firebase

--- Agenda/src/routes/index.js
+++ Agenda/src/routes/index.js
@@ -1,14 +1,28 @@
- express = require("express");
+express = require("express");
+
+ //LOGIN
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+
+const csrfMiddleware = csrf({cookie:true});
 
 const app = express();
 var exphbs = require('express-handlebars')
 var bodyParser= require('body-parser')
+
+//LOGIN
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(csrfMiddleware);
+
+
 
 app.use(bodyParser.urlencoded({extended: true }))
 app.use(express.static('public'))
 
 //cambie variable
 const admin =  require('firebase-admin');
+
 
 var serviceAccount = require("/workspace/Docker_NodeJS/Agenda/agenda2-1930b-firebase-adminsdk-wv3ia-21e2881362.json");
 
@@ -19,7 +33,71 @@ admin.initializeApp({
 
 const db = admin.database();
 
-app.get('/', (req, res) => {
+//LOGIN
+app.all("*", (req, res, next) => {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  next();
+});
+
+app.get("/login", function (req, res) {
+  res.render("login.hbs");
+});
+
+app.get("/signup", function (req, res) {
+  res.render("signup.hbs");
+});
+
+app.get("/profile", function (req, res) {
+  const sessionCookie = req.cookies.session || "";
+
+  admin.auth().verifySessionCookie(sessionCookie,true)
+    .then(() => {
+      res.render("profile.hbs");
+    })
+    .catch((error) => {
+      res.redirect("/login");
+    });
+});
+
+app.get("/", function (req, res) {
+  res.render("index2.hbs");
+});
+
+app.get('/sessionLogin',(req,res) => {
+  res.clearCookie("session")
+  res.redirect('/login')
+});
+
+app.post("/sessionLogin", (req, res) => {
+  const idToken = req.body.idToken.toString();
+
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+  admin
+    .auth()
+    .createSessionCookie(idToken, { expiresIn })
+    .then(
+      (sessionCookie) => {
+        const options = { maxAge: expiresIn, httpOnly: true };
+        res.cookie("session", sessionCookie, options);
+        res.end(JSON.stringify({ status: "success" }));
+      },
+      (error) => {
+        res.status(401).send("UNAUTHORIZED REQUEST!");
+        res.redirect('/login')
+      }
+    );
+});
+
+app.get("/sessionLogout", (req, res) => {
+  res.clearCookie("session");
+  res.redirect("/login");
+});
+
+
+
+//AGENDA
+app.get('/agenda', (req, res) => {
     db.ref('agenda').once('value', (snapshot) => {
         const data = snapshot.val();
         res.render('index.hbs', { agenda: data });
@@ -40,12 +118,12 @@ app.get('/', (req, res) => {
 app.post('/guardar', (req, res) => {
     console.log(req.body);
     db.ref('agenda').push(req.body);
-    res.redirect('/');
+    res.redirect('/agenda');
 });
 
 app.get('/delete/:id', (req, res) => {
     db.ref('agenda/' + req.params.id).remove();
-    res.redirect('/');
+    res.redirect('/agenda');
 });
 
 app.listen(3000, () => {
@@ -58,7 +136,7 @@ module.exports = app;
 /*const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => { 
+app.get('/', (req, res) => { 
     res.send("<h1>Hello from Node, from inside a docker container...</h1>");
     console.log("App running on port 3000...");
 });*/
--- README.md
+++ README.md
@@ -6,8 +6,9 @@ docker build -t agenda:v1 .
 # Crear contenedor
 docker run -it -p 3000:3000 -v /workspace/Docker_NodeJS/Agenda:/Agenda --name agenda -h agenda agenda:v1
 
+# Firebase
 $env:GOOGLE_APPLICATION_CREDENTIALS="/workspace/Docker_NodeJS/Agenda/agenda2-1930b-firebase-adminsdk-wv3ia-21e2881362.json"
 
 export GOOGLE_APPLICATION_CREDENTIALS="/workspace/Docker_NodeJS/Agenda/agenda2-1930b-firebase-adminsdk-wv3ia-21e2881362.json"
 
-/workspace/Docker_NodeJS/Agenda/agenda2-1930b-firebase-adminsdk-wv3ia-21e2881362.json
+/workspace/Docker_NodeJS/Agenda/agenda2-1930b-firebase-adminsdk-wv3ia-21e2881362.
